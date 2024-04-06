4375814c23e91544d0b66d7d3f833ef963897f3b
luke <1156953217@qq.com>
2024-04-06 13:26:04 +0800

    🐞 fix: rename page

--- app/[lang]/page.js
+++ app/[lang]/page.js
@@ -1,9 +1,9 @@
 import { getDictionary } from "@/i18n/get-dictionary";
-import LocaleSwitcher from "@/components/navbar/locale-switcher";
-import ThemeSwitcher from "@/components/navbar/theme-switcher";
-import IndexPage from "@/components/form/indexPage";
-import IndexTable from "@/components/form/indexTable";
-import IndexFooters from "@/components/footer/indexFooters";
+import {LocaleSwitcher} from "@/components/navbar/locale-switcher";
+import {ThemeSwitcher} from "@/components/navbar/theme-switcher";
+import {IndexPage } from "@/components/form/indexPage";
+import {IndexTable}  from "@/components/form/indexTable";
+import {IndexFooters}  from "@/components/footer/indexFooters";
 
 export default async function Home({ params: { lang } }) {
   const dictionary = await getDictionary(lang);
